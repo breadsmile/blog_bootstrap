8feb2c244ade91d6a34791def2ce6e4904f30db3
zhukejun <zhukejun78@hotmail.com>
2014-04-03 15:24:09 +0800

    zkj

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@
 # the following line to use "https"
 source 'http://rubygems.org'
 
+
 gem "middleman", "~> 3.2.2"
 gem "middleman-blog", "~> 3.5.1"
 gem "middleman-target", "~> 0.0.6"
--- config.rb
+++ config.rb
@@ -109,8 +109,8 @@ set :site_title, 'Blog title'
 set :site_description, 'Blog description'
 # Select the theme from bootswatch.com.
 # If false, you can get plain bootstrap style.
-# set :theme_name, 'flatly'
-set :theme_name, false
+ set :theme_name, 'custom'
+#set :theme_name, false
 # set @analytics_account, like "XX-12345678-9"
 @analytics_account = false
 
